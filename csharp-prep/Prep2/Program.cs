cccff55eaba10be020147894f154f35b06b03aa0
Michaelsoft-Riley <ril22002@byui.edu>
2023-09-16 10:30:25 -0600

    Added lettergrade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.ComponentModel;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage grade? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string letter;
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass. You'll get it next time!");
+        }
+        
+        Console.WriteLine($"The letter grade is {letter}");
     }
 }
