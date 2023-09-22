45001195d7a2d81ed7f84daedeee1d0201f74793
Michaelsoft-Riley <ril22002@byui.edu>
2023-09-21 22:05:07 -0600

    Loop Practice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int randNum = random.Next(0, 50);
+        Console.WriteLine(randNum);
+        int guessInt;
+        
+        do
+        {
+            Console.Write("Guess the number(0-50): ");
+            guessInt = int.Parse(Console.ReadLine());
+
+            if(guessInt == randNum)
+            {
+                Console.WriteLine("You got it!");
+            }
+            else
+            {
+                Console.WriteLine("Nope! Try again!");
+            }
+        } while(guessInt != randNum);
     }
 }
