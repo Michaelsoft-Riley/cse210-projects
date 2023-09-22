7a9b842139de46eb3da6e3b4d58838ea71ed0e1d
Michaelsoft-Riley <ril22002@byui.edu>
2023-09-22 01:53:39 -0600

    Practiced writing functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNum = PromptUserNumber();
+        int squareNum = SquareNumber(favNum);
+        DisplayResult(userName, squareNum);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+    static int SquareNumber(int favNum)
+    {
+        int squareNum = favNum * favNum;
+        return squareNum;
+    }
+    static void DisplayResult(string name, int squareNum)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squareNum}");
     }
 }
