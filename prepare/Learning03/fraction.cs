0a91a31d0cdbd0fdb7273cbdd64b60989b844493
Michaelsoft-Riley <ril22002@byui.edu>
2023-10-11 23:36:13 -0600

    Encapsulation with getters and setters practice

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,57 @@
+using System;
+
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public string GetFraction()
+    {
+        string fraction = $"{_top}/{_bottom}";
+        return fraction;
+    }
+
+    public double GetDecimal()
+    {
+        return (double)_top / _bottom;
+    }
+}
