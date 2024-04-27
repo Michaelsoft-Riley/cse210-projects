e01a0022d99559a3b115fcc6dbe28666b5ca4668
Michaelsoft-Riley <ril22002@byui.edu>
2024-04-27 14:20:36 -0600

    Added list practice calculator

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+        int sum = 0;
+        int largest = 0;
+        int smallest;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Fill list with user input
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }    
+        } while (number != 0);
+        
+        // Calculate largest and sum. Set smallest to a large value
+        largest = numbers.Max();
+        smallest = largest;
+        sum = numbers.Sum();
+
+        // Find smallest
+        foreach (int n in numbers)
+        {
+            if (n > 0 && n < smallest)
+            {
+                smallest = n;
+            }
+        }
+
+        // Calculate average
+        int length = numbers.Count;
+        float average = (float)sum / length;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int i in numbers)
+        {Console.WriteLine(i);}
     }
 }
