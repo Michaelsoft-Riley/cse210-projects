4eb31893953e011930527104d18066f4472c1aad
Michaelsoft-Riley <ril22002@byui.edu>
2024-04-30 16:12:59 -0600

    Practice with classes: resume program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Michaelsoft";
+        job1._jobTitle = "Lv.3 Cheeselord";
+        job1._startYear = 1992;
+        job1._endYear = 2006;
+
+        Job job2 = new Job();
+        job2._company = "Moogle";
+        job2._jobTitle = "Professional Monster Slicer";
+        job2._startYear = 1998;
+        job2._endYear = 2003;
+
+        Resume resume = new Resume();
+        resume._name = "Bob";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
