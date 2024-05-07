8740cf26880959d69f28722582a7e5a68e90f897
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-06 22:47:43 -0600

    Initial Version

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(3, 5);
+
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f3.GetFractionString());
     }
 }
