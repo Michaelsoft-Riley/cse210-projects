1d6684b697f6b4a963163243326a6fe426673121
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-08 22:31:15 -0600

    Added methods, attributes, and a constructor to word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,2 +1,23 @@
 using System;
 
+public class Word
+{
+    private string _text;
+    private bool _hidden;
+
+    public Word(string wordText)
+    {
+        _text = wordText;
+        _hidden = false;
+    }
+
+    public void Hide()
+    {
+        _hidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _hidden;
+    }
+}
