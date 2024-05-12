9de7a8303d08a37b74d3aafd1f06c5bd354cdeac
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-11 23:44:09 -0600

    Added basic functionality

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
 class Program
+// Added the option to select from any verse in the book of mormon.
+// Added a class for collecting verses from a .txt version of the Book of Mormon obtained from Project Gutenberg.
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Enter a scripture in 'book chapter:verse-endVerse' format.");
+        Console.WriteLine("(Ex: 1Nephi 1:1) or (Ex: 1Nephi 1:1-2)");
+        string response = Console.ReadLine();
+
+        Scripture scripture = new Scripture(response);
+
+        while (response != "quit")
+        {
+            Console.Clear();
+            scripture.DisplayScripture();
+
+            if (scripture.IsAllHidden())
+            {
+                break;
+            }
+
+            Console.Write($"\nPress enter to continue or type 'quit' to finish: ");
+            response = Console.ReadLine();
+
+            scripture.HideRandomWords();
+        }
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,5 +2,75 @@ using System;
 
 public class Scripture
 {
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
 
+    public Scripture(string userReference)
+    {
+        BookofMormonProcessor bookofMormonProcessor = new();
+
+        _reference = new Reference(userReference);
+        List<string> references = _reference.GetVersesList();
+        foreach (string verseReference in references)
+        {
+            string verse = bookofMormonProcessor.GetVerse(verseReference);
+            VerseToWord(verse);
+        }
+    }
+
+    private void VerseToWord(string text)
+    // Stores each word in a string as a Word instance
+    {
+        string[] words = text.Split();
+        foreach (string word in words)
+        {
+            Word newWord = new(word);
+            _words.Add(newWord);
+        }
+    }
+
+    public void DisplayScripture()
+    {
+        Console.Clear();
+
+        string referenceText = _reference.GetDisplayText();
+        string DisplayString = $"{referenceText} ";
+
+        foreach (Word word in _words)
+        {
+            if (word.IsHidden())
+            {
+                DisplayString += $"{new string('_', word.GetText().Length) } ";
+            }
+            else
+            {
+                DisplayString += $"{word.GetText()} ";
+            }
+        }
+        Console.WriteLine(DisplayString);
+    }
+
+    public void HideRandomWords()
+    // TODO: Exclude punctuation
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Random random = new Random();
+            int randomNum = random.Next(_words.Count());
+
+            _words[randomNum].Hide();
+        }
+    }
+
+    public bool IsAllHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
