fa56811f01005e2319a73bf7123dc68cd85ea3b2
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-14 00:10:29 -0600

    Added display

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Nelson", "Getting Stuff Done Pt3");
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment mathAssignment= new MathAssignment("Johnathon", "Derivatives", "7.3", "20-21");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment= new WritingAssignment("Billy", "Humor 111", "The Great Cheese Catastrophe");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
