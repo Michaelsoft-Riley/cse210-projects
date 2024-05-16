fdca8a723104b45fec8c0d062ac36a5f0c921e6d
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-16 15:44:44 -0600

    Added StartMessage and spinning animation. StartMessage now recieves user input for duration directly instead of recieving it from the child classes.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -6,16 +6,25 @@ public class Activity
     private string _description;
     private int _duration;
 
-    public Activity(int duration, string name, string description)
+    public Activity(string name, string description)
     {
         _name = name;
         _description = description;
-        _duration = duration;
+
+        StartMessage();
     }
 
     public void StartMessage()
     {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name}.");
+        Console.WriteLine($"\n_description");
+        Console.Write("\nHow long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
 
+        Console.Clear();
+        Console.WriteLine("Get ready...");
+        SpinningAnimation();
     }
 
     public void EndMessage()
@@ -30,6 +39,16 @@ public class Activity
 
     public void SpinningAnimation()
     {
+        List<char> chars = new List<char>()
+        {
+            '/', '-', '\\', '|'
+        };
 
+        foreach (char c in chars)
+        {
+        Console.Write("\b \b");
+        Console.Write(c);
+        Thread.Sleep(2500);
+        }
     }
 }
