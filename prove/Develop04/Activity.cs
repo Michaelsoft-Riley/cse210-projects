2573a5f9690f7d40249b711ac7f87f13dec96c25
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-16 16:02:39 -0600

    Fixed a typo in StartMessage. Changed Spinning Animation speed and set it to loop 5 times.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -18,7 +18,7 @@ public class Activity
     {
         Console.Clear();
         Console.WriteLine($"Welcome to the {_name}.");
-        Console.WriteLine($"\n_description");
+        Console.WriteLine($"\n{_description}");
         Console.Write("\nHow long, in seconds, would you like for your session? ");
         _duration = int.Parse(Console.ReadLine());
 
@@ -44,11 +44,12 @@ public class Activity
             '/', '-', '\\', '|'
         };
 
-        foreach (char c in chars)
-        {
-        Console.Write("\b \b");
-        Console.Write(c);
-        Thread.Sleep(2500);
-        }
+        for (int i = 0; i < 6; i++)
+            foreach (char c in chars)
+            {
+            Console.Write("\b \b");
+            Console.Write(c);
+            Thread.Sleep(300);
+            }
     }
 }
