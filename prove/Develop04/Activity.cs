3265df88ac8796f6db9a6ca361d056c26ea9f015
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-18 20:29:25 -0600

    Added _count, _index, LoadCount(), and UpdateCount()

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -7,7 +7,9 @@ public abstract class Activity
     private int _duration;
 
     // number of times that the activity is completed
-    private int _count;
+    private int _count = 0;
+    // used when saving and loading the count
+    private int _index;
 
     public Activity(string name, string description)
     {
@@ -74,9 +76,34 @@ public abstract class Activity
         return _duration;
     }
 
+    public void SetIndex(int index)
+    {
+        _index = index;
+    }
+
     public int GetCount()
     {
         return _count;
+    }
+
+    public void UpdateCount()
+    // This will increase _count by one for each time the activity is ran
+    {
+        _count += 1;
+    }
+
+    public void LoadCount()
+    // Load an activity's count from file using the class's index value
+    {
+        int totalCount = 0;
+        string[] lines = File.ReadAllLines("count.txt");
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(",");
+            // TODO: Make sure that this doesn't throw a type error
+            totalCount += int.Parse(parts[_index]);  
+        }
+        _count = totalCount;
     }
 
     public abstract void Run();
