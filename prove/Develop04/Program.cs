bf7317be6f03d8d3e70edbcfdb44c684ef2919bd
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-18 20:33:39 -0600

    Added LoadCount for all classes at the start of the program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
+// Stretch Challenge: Program now counts, saves, and loads the number of times each activity has been completed
 class Program
 {
     static void Main(string[] args)
     {
+        BreathingActivity breathingActivity = new BreathingActivity();
+        ReflectionActivity reflectionActivity = new ReflectionActivity();
+        ListingActivity listingActivity = new ListingActivity();
+
+        breathingActivity.LoadCount();
+        reflectionActivity.LoadCount();
+        listingActivity.LoadCount();
+
         // Menu
         string choice = "";
         while (choice != "4")
@@ -14,28 +23,29 @@ class Program
             Console.WriteLine("   1. Start breathing activity");
             Console.WriteLine("   2. Start reflecting activity");
             Console.WriteLine("   3. Start listing activity");
-            Console.WriteLine("   4. Quit");
+            Console.WriteLine("   4. Save and Quit");
             Console.Write("Select a choice from the menu: ");
             choice = Console.ReadLine();
 
             if (choice == "1")
             {
-                BreathingActivity breathingActivity = new BreathingActivity();
                 breathingActivity.Run();
             }
             else if (choice == "2")
             {
-                ReflectionActivity reflectionActivity = new ReflectionActivity();
                 reflectionActivity.Run();
             }
             else if (choice == "3")
             {
-                ListingActivity listingActivity = new ListingActivity();
                 listingActivity.Run();
             }
             else if (choice == "4")
             {
-
+                // Save the number of times each activity has been completed to a file
+                using (StreamWriter file = new StreamWriter("count.txt"))
+                {
+                    file.WriteLine($"{breathingActivity.GetCount()},{reflectionActivity.GetCount()},{listingActivity.GetCount()}");
+                }
             }
         }
     }
