e0f2f0956195d7e34fc060003d00d2474d8c4966
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-21 00:13:59 -0600

    Final Version

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square(3, "green"));
+        shapes.Add(new Rectangle(3, 4, "orange"));
+        shapes.Add(new Circle(3, "pink"));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Color: {color}   Area: {area}");
+        }
     }
 }
