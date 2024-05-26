be95dfc5d916761023c9de17fc5799293c7b88b5
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-25 22:49:37 -0600

    Added comment describing "Creativity and Exceeding Requirements" additions

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,6 +1,8 @@
 using System;
 
 class Program
+// Added a recurring checklist goal that resets its completion count to 1 once it is ran after
+// reaching its target count. This class inherits from ChecklistGoal.
 {
     static void Main(string[] args)
     {
