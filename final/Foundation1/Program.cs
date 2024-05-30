7ba9272f730f3760c1edba4ee723e78c2f979133
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-29 19:42:53 -0600

    Added display video details

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new();
+        Video v1 = new("Thing That Happened!", "IceCreamSandwich", 300);
+        v1.AddComment("Tim", "Wow, that's crazy!");
+        v1.AddComment("Bill", "So cool");
+        v1.AddComment("John", "alskdjfh");
+        videos.Add(v1);
+
+        Video v2 = new("I Blew Up a Thing!", "DudeWithGun", 900);
+        v2.AddComment("Tim", "Wow, that's crazy!");
+        v2.AddComment("Bill", "So cool");
+        v2.AddComment("John", "alskdjfh");
+        videos.Add(v2);
+
+        Video v3 = new("Don't Do This When Visiting!", "PersoninAnotherCountry", 1200);
+        v3.AddComment("Tim", "Wow, that's crazy!");
+        v3.AddComment("Bill", "So cool");
+        v3.AddComment("John", "alskdjfh");
+        videos.Add(v3);
+
+        foreach (Video v in videos)
+        {
+            v.Display();
+        }
     }
 }
