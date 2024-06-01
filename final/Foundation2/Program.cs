4d540ceb074d7bd40ddd30d4f9994d65706335e0
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-31 19:41:40 -0600

    Added Bob's order

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        List<Order> orders = new();
+        Order order;
+
+        order = new("Jeff", "1475 N Main St,Logan,UT,USA");
+        order.AddProduct("Pencil", "324A", .33, 3);
+        order.AddProduct("Eraser", "G352", .50, 2);
+        orders.Add(order);
+
+        order = new("Jeremiah", "1300 N Main St,Logan,UT,USA");
+        order.AddProduct("Pencil", "324A", .33, 2);
+        order.AddProduct("Paper", "L33T", .10, 10);
+        orders.Add(order);
+
+        order = new("Bob", "85 Yangzhai Rd,Changning District,Shanghai,China");
+        order.AddProduct("Chopsticks", "674A", .77, 4);
+        order.AddProduct("Paper", "L33T", .10, 10);
+        orders.Add(order);
+
+        int orderCount = 1;
+        foreach (Order o in orders)
+        {
+            double total = o.GetTotal();
+            Console.WriteLine($"Order{orderCount} Total Cost: ${total}");
+
+            o.DisplayPackagingLabel();
+            o.DisplayShippingLabel();
+            orderCount ++;
+        }
     }
 }
