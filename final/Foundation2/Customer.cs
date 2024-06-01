51e2c90353dfa20789608b358adf23305610eac5
Michaelsoft-Riley <ril22002@byui.edu>
2024-05-31 18:13:43 -0600

    Added Customer

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,21 @@
+public class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, string addressString)
+    {
+        _name = name;
+        _address = new(addressString);
+    }
+
+    public bool IsUSA()
+    {
+        return _address.IsUSA();
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+}
