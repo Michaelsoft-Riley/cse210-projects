3b9c5056d8bed53b1d36fa682985c246cf4d0c39
Michaelsoft-Riley <ril22002@byui.edu>
2024-06-02 15:03:23 -0600

    Simplified activities initialization

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,11 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Activity> activities = new();
-
-        activities.Add(new RunningActivity("5/3/2023", 90, 3));
-        activities.Add(new BikingActivity("5/4/2023", 60, 15));
-        activities.Add(new SwimmingActivity("5/5/2023", 20, 5));
+        List<Activity> activities =
+        [
+            new RunningActivity("5/3/2023", 90, 3),
+            new BikingActivity("5/4/2023", 60, 15),
+            new SwimmingActivity("5/5/2023", 20, 5),
+        ];
 
         foreach (Activity activity in activities)
         {
