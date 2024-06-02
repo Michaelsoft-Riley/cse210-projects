ce1d59ac66f4fa2dbf016bc42a00c0ec72aa0531
Michaelsoft-Riley <ril22002@byui.edu>
2024-06-02 15:00:13 -0600

    Version 1.0

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new();
+
+        activities.Add(new RunningActivity("5/3/2023", 90, 3));
+        activities.Add(new BikingActivity("5/4/2023", 60, 15));
+        activities.Add(new SwimmingActivity("5/5/2023", 20, 5));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
